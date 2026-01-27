b0d34038d39a72a22fcc0c9257379798a04b5da7
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2026-01-27 18:21:54 +0100

    Account Class

--- model/Account.java
+++ model/Account.java
@@ -1,5 +1,68 @@
 package model;
+/*
+### Champs à mettre dans `Account`
 
-public class Account {
-    
+* `id` (String ou int)
+* `ownerName` (String)
+* `balance` (double)
+
+### Méthodes dans `Account`
+
+* `deposit(double amount)`
+* `withdraw(double amount)` (peut être abstraite si tu veux forcer les règles par type)
+* `toString()` (affichage propre)
+
+💡 **Révision** :
+
+* `private` champs + `public` getters/setters (encapsulation)
+* `protected` si tu veux accessible aux classes filles.
+
+---
+*/
+public abstract class Account {
+    private int id;
+    private String ownerName;
+    private double balance;
+    public Account(){
+        this.id = 0;
+        this.ownerName = "Null";
+        this.balance = 0.0;
+    }
+    public Account(int id,String ownerName,double balance){
+        this.id = id;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+    public Account(Account other){
+        this.id = other.id;
+        this.ownerName = other.ownerName;
+        this.balance = other.balance;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getOwnerName(){
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName){
+        this.ownerName = ownerName;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+    @Override
+    public String toString(){
+        return( " Id ==> "+ id +
+            " Owner Name ==> "+ ownerName +
+            " Balance ==> " + balance
+        );
+    }
 }
